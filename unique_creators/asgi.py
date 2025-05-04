935f3e327bd2d1be06ab661cfb4a415d00fc7921
nadir <nadirali7q12@gmail.com>
2025-05-04 13:53:59 +0500

    initial commit

---
+++ unique_creators/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unique_creators project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unique_creators.settings')
+
+application = get_asgi_application()
